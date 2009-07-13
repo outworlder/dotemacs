a16fb8be61d9eca088e4697e70348d46b81bc65f
Stephen Eilert <stephen@atlantico2354.(none)>
2009-07-13 17:40:46 -0300

    - Adding the ability to read Atom feeds (converted using XSLT).

--- children/gnus.el
+++ children/gnus.el
@@ -6,3 +6,19 @@
  message-default-charset `utf-8
  nnml-directory "~/Mail/"
  gnus-select-method '(nnml ""))
+
+(require 'mm-url)
+(defadvice mm-url-insert (after DE-convert-atom-to-rss () )
+  "Converts atom to RSS by calling xsltproc."
+  (when (re-search-forward "xmlns=\"http://www.w3.org/.*/Atom\"" 
+			   nil t)
+    (goto-char (point-min))
+    (message "Converting Atom to RSS... ")
+    (call-process-region (point-min) (point-max) 
+			 "xsltproc" 
+			 t t nil 
+			 (expand-file-name "~/.emacs.children/support/atom2rss.xsl") "-")
+    (goto-char (point-min))
+    (message "Converting Atom to RSS... done")))
+
+(ad-activate 'mm-url-insert)
