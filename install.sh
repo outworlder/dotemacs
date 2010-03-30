b27d0015e7b31142765de7e64d30d0d640b99f39
Stephen Eilert <stephen@Arcturus.(none)>
2010-03-30 12:06:47 -0300

    - The install script was not working. Added a snippet for it to correctly figure out the script directory.

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# From http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-in
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
 echo "Creating symlinks"
 
-ln -s .emacs ~/.emacs
-ln -s .emacs.children ~/children
+ln -s $SCRIPT_PATH/.emacs ~/.emacs
+ln -s $SCRIPT_PATH/children ~/.emacs.children
 
 echo "Done."
