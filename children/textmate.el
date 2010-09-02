89cb82f31e76c158594bb8c07b462b6740b42fde
Stephen Eilert <spedrosa@gmail.com>
2010-09-02 20:31:10 -0300

    - Adding autopair examples

--- children/textmate.el
+++ children/textmate.el
@@ -1,10 +1,15 @@
-
 (require 'textmate)
 (tm/initialize)
 
 ;; (define-skeleton ruby-hash-skeleton
 ;;   "Inserts a ruby hash with brackets. TODO: Check if inside a string" nil
 ;;   ?#?{ _ ?})
+;; ;
+;; ;; (add-hook 'ruby-mode-hook
+;; ;; 	  (set-key "#" 'ruby-hash-skeleton))
 
-;; (add-hook 'ruby-mode-hook
-;; 	  (set-key "#" 'ruby-hash-skeleton))
+;; (require 'autopair)
+;; (autopair-global-mode)
+;; (setq autopair-autowrap t)
+;; (setq autopair-blink t)
+
