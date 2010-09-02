24ebbcd3153fcbc12953b259075b8d181980bf13
Stephen Eilert <spedrosa@gmail.com>
2010-09-02 20:38:28 -0300

    - Adding parenthesis highlighting for all lisp-like modes.

--- children/elisp.el
+++ children/elisp.el
@@ -8,4 +8,5 @@
 (add-hook 'emacs-lisp-mode-hook
           '(lambda ()
              (modify-syntax-entry ?- "w")       ; now '-' is not considered a word-delimiter
+	     (highlight-parentheses-mode t)
              ))
