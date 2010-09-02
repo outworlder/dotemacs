238765307a0852c868ad0014e298c43919168293
Stephen Eilert <spedrosa@gmail.com>
2010-09-02 16:40:04 -0300

    - Temporarily removing the ruby hash insertion

--- children/textmate.el
+++ children/textmate.el
@@ -1,2 +1,10 @@
+
 (require 'textmate)
 (tm/initialize)
+
+;; (define-skeleton ruby-hash-skeleton
+;;   "Inserts a ruby hash with brackets. TODO: Check if inside a string" nil
+;;   ?#?{ _ ?})
+
+;; (add-hook 'ruby-mode-hook
+;; 	  (set-key "#" 'ruby-hash-skeleton))
