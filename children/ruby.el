5bf4ae7922e40432a0c58f6844b9183a66ef7c56
Stephen Eilert <spedrosa@gmail.com>
2010-09-06 14:08:06 -0300

    - Added a <C-'> shortcut to insert "=>"

--- children/ruby.el
+++ children/ruby.el
@@ -190,18 +190,24 @@
 (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 
 (defun ruby-insert-hash-string ()
-  (interactive)
   "Inserts a #{} but only if inside a string"
+  (interactive)
   (let ((stringp (fourth (syntax-ppss)))
 	(skeleton-end-newline nil))
     (if stringp
 	(skeleton-insert '(nil ?# ?{ _ ?} nil))
       (skeleton-insert '(nil ?# _)))))
 
+(defun ruby-insert-=> ()
+  "Inserts a =>"
+  (interactive)
+  (let ((skeleton-end-newline nil))
+    (skeleton-insert '(nil " => "))))
+
 (add-hook 'ruby-mode-hook
 	  (lambda ()
-	    (local-set-key "#" 'ruby-insert-hash-string)))
-
+	    (local-set-key "#" 'ruby-insert-hash-string)
+	    (local-set-key (read-kbd-macro "^'") 'ruby-insert-=>)))
 
 (add-hook 'ruby-mode-hook
           (lambda()
