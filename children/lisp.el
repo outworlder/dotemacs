93574d7ab8773d1be11391221adc5ba82ad0dce9
Stephen <spedrosa@gmail.com>
2010-11-21 14:11:00 -0300

    - Slime configuration.

--- children/lisp.el
+++ children/lisp.el
@@ -4,4 +4,9 @@
 (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
 ;; Optionally, specify the lisp program you are using. Default is "lisp"
 (setq inferior-lisp-program "sbcl --control-stack-size 32")
+(require 'slime)
 (slime-setup '(slime-fancy slime-asdf))
+
+
+(setq org-agenda-files (list "~/Documents/Projects/citycouncil/citycouncil.org"))
+(setq org-log-done 'time)
